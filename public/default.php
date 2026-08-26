417c1e6ca98183222feba4880ee761ea5c8dbedd
nithinvikrin <nithin.sanda@vikrin.com>
2026-08-26 14:41:34 +0530

    Fix Hostinger deployment routing and remove default pages

--- public/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Overwrite Hostinger default placeholder page and load React SPA index.html
-if (file_exists(__DIR__ . '/index.html')) {
-    include __DIR__ . '/index.html';
-} else {
-    header("Location: /");
-}
-exit();
-?>
